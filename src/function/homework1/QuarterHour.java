da93b624474ac0a9a88cc7a0592e53930f916420
Aleksandr Bobylev <alexander.bobylev.88@gmail.com>
2021-11-10 09:46:20 +0300

    Change .gitignore, QuarterHour.java, CompareSquareTriangle.java

--- src/function/homework1/QuarterHour.java
+++ src/function/homework1/QuarterHour.java
@@ -1,4 +1,29 @@
 package function.homework1;
 
+/**
+ * В переменной minutes лежит число от 0 до 59.
+ * Написать функцию, которая принимает в качестве параметра значение переменной minutes и выводит на консоль
+ * в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+ * Протестировать функцию в main.
+ */
 public class QuarterHour {
+    public static void main(String[] args) {
+        int minutes = 20;
+        quarter(minutes );
+    }
+
+    public static void quarter(int minutes) {
+        if (minutes >= 0 && minutes <= 15) {
+            System.out.println("В первую четверть.");
+        }
+        if (minutes >= 16 && minutes <= 30) {
+            System.out.println("Во вторую четверть.");
+        }
+        if (minutes >= 31 && minutes <= 45) {
+            System.out.println("В третью четверть.");
+        }
+        if (minutes >= 46 && minutes <= 59) {
+            System.out.println("В четвертую четверть.");
+        }
+    }
 }
--- src/function/homework3/CompareSquareTriangle.java
+++ src/function/homework3/CompareSquareTriangle.java
@@ -4,4 +4,5 @@ public class CompareSquareTriangle {
     public static void main(String[] args) {
 
     }
+
 }
