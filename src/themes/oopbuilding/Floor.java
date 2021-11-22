b572dbd1bfe52d65496dbe892250f9c52d995b81
Aleksandr Bobylev <alexander.bobylev.88@gmail.com>
2021-11-22 14:37:07 +0300

    Add House task

--- src/themes/oopbuilding/Floor.java
+++ src/themes/oopbuilding/Floor.java
@@ -1,22 +1,20 @@
 package themes.oopbuilding;
 
 public class Floor {
-    private int numberFloor;
-    private int[] apartments;
 
-    public Floor(int numberFloor, int[] apartments) {
+    private final int numberFloor;
+    private final Apartment[] apartments;
+
+    public Floor(int numberFloor, Apartment[] apartments) {
         this.numberFloor = numberFloor;
         this.apartments = apartments;
     }
 
-    public int getNumberFloor() {
-        return numberFloor;
+    public Apartment[] getApartments() {
+        return apartments;
     }
 
-    public int getApartments() {
-        return apartments.length;
-    }
     public void printFloor() {
-        System.out.println("Номер этажа " + getNumberFloor() + "\nКоличество квартир: " + getApartments());
+        System.out.println("Номер этажа " + numberFloor + "\nКоличество квартир: " + apartments.length);
     }
 }
